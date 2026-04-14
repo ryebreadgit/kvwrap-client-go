af1016a91e9950b6cef8a6b1411102edb8170ba4
RyeBread <rye@rye.moe>
2026-04-14 10:57:05 -0600

    Rename package, add errors, add partition requirement, add context requirement

--- kvwrap.go
+++ kvwrap.go
@@ -1,7 +1,15 @@
-package kvwrapclient
+package kvwrap
 
 import (
 	"context"
+	"fmt"
+)
+
+var (
+	ErrKeyNotFound      = fmt.Errorf("key not found")
+	ErrInvalidPartition = fmt.Errorf("invalid partition")
+	ErrInvalidKey       = fmt.Errorf("invalid key")
+	ErrInvalidValue     = fmt.Errorf("invalid value")
 )
 
 type KVPair struct {
@@ -18,11 +26,11 @@ type WatchEvent struct {
 
 type KVWrapClient interface {
 	// Get retrieves the value associated with the given key.
-	Get(key []byte) ([]byte, error)
+	Get(ctx context.Context, partition string, key []byte) ([]byte, error)
 	// Set sets the value for the given key.
-	Set(key []byte, value []byte) error
+	Set(ctx context.Context, partition string, key []byte, value []byte) error
 	// Delete removes the key-value pair associated with the given key.
-	Delete(key []byte) error
+	Delete(ctx context.Context, partition string, key []byte) error
 	// Scan retrieves all key-value pairs that match the given optional prefix.
 	Scan(ctx context.Context, partition string, prefix []byte) (<-chan KVPair, <-chan error)
 	// WatchKey watches for changes to a specific key and sends events to the provided channel.
@@ -30,7 +38,7 @@ type KVWrapClient interface {
 	// WatchPrefix watches for changes to keys with a specific prefix and sends events to the provided channel.
 	WatchPrefix(ctx context.Context, partition string, prefix []byte) (<-chan WatchEvent, <-chan error)
 	// GetJSON retrieves the value associated with the given key and unmarshals it into the provided struct.
-	GetJSON(key []byte, value any) error
+	GetJSON(ctx context.Context, partition string, key []byte, value any) error
 	// SetJSON marshals the provided struct and sets it as the value for the given key.
-	SetJSON(key []byte, value any) error
+	SetJSON(ctx context.Context, partition string, key []byte, value any) error
 }
